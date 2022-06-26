f2a0ed69bc965cbe4575fc73c6165a67262929fb
khalifahWaleed <99013828+khalifahWaleed@users.noreply.github.com>
2022-06-26 19:43:42 +0300

    Add files via upload

---
+++ html&php.php
@@ -0,0 +1,33 @@
+<?php
+      $b="<details> ";
+      $b .="<summary>";
+      
+      $b .="name";
+      $b .="</summary>";
+       // $x="Khalifah" . " ali" . " gamal ". " saleh";
+      $b .="ahmed";
+      $b .=" ali";
+      $b .=" Khalifah";
+      $b .=" Saleh";
+      $b .="</details> ";
+?>
+
+<?php
+echo $b;
+?>
+
+
+<!--<details>
+    <summary><?php echo $a ?></summary>
+  //<?php
+  //echo $x;
+  //echo $b;
+  
+  ?>
+    
+    
+</details>
+    -->
+    
+    
+
